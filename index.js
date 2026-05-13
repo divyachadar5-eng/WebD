6b0091bb44c7c03557f9eec6d735cd46f37b1135
Divya Chadar <divyachadar5@gmail.com>
2026-05-13 15:49:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector("input")
+
+let inp=document.querySelector("input")
+let h3= document.getElementsByClassName("h2")
+let btn=document.querySelector("button")
+console.log(btn,"heheh");
+
+btn.addEventListener("click",function(){
+    console.log("hello");
+    let data=inp.value
+    
+
+})
+
+// inp.addEventListener("input",function(e)){
+//     console.log(e.target.value);
+//     h2.innerText=e.target.value
+// }
